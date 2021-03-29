2a34902705704da4b2f55592e41f031d27b1c7d8
Caleb Denio <cjdenio44@gmail.com>
2021-03-29 14:14:59 -0400

    ✨ Add config.ts and init Bolt app

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import { DateTime } from "luxon";
+
+export default {
+  // Channels that require Hack Club Gold
+  blockedChannels: [],
+  startDate: DateTime.utc(2021, 4, 1, 0, 0, 0, 0),
+  endDate: DateTime.utc(2021, 4, 2, 0, 0, 0, 0),
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { App } from "@slack/bolt";
+
 console.log("Hello world 🌎");
+
+const app = new App({
+  signingSecret: process.env.SIGNING_SECRET,
+  token: process.env.TOKEN,
+});
+
+(async () => {
+  await app.start(parseInt(process.env.PORT as string) || 3000);
+  console.log("App started");
+})();
